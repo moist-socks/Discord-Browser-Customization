c07e7be6d1371f9be758c64d1444bcdbc8867209
shocz-lol <76273033+shocz-lol@users.noreply.github.com>
2022-03-26 14:56:37 -0500

    change prompt

--- backgroundcolormessages.js
+++ backgroundcolormessages.js
@@ -1,5 +1,5 @@
 (function() {
   var a = document.querySelector('.chatContent-3KubbW');
-  var b = prompt("Hex code of color or color name according to css","");
+  var b = prompt("Hex code of color (include #) or color name according to CSS","");
   a.style.backgroundColor = b;
 })();
