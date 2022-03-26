fd25fb8888394c7db5d9a39488f9de4abcb69f1f
shocz-lol <76273033+shocz-lol@users.noreply.github.com>
2022-03-26 16:02:45 -0500

    Update cursedbackground.js

--- cursedbackground.js
+++ cursedbackground.js
@@ -1,3 +1,10 @@
+/* 
+IMPORTANT HOW TO USE
+DON'T COPY THIS PART
+
+Send a message of the image you want for your background. Then click on that image. Right click once the image is enlarged. Click copy image link. Once you do that run this script
+*/
+
 (function() {
   var a = document.querySelector('.erd_scroll_detection_container erd_scroll_detection_container_animation_active');
   var b = prompt("The message link you copied before running this script","");
