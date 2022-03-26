4066ca8c092be728bb0a2d170c1ca1199ca08308
shocz-lol <76273033+shocz-lol@users.noreply.github.com>
2022-03-26 15:33:24 -0500

    Update backgroundimagemessages.js
    Send a message of the image you want for your background. Then click on that image. Right click once the image is enlarged. Click copy image link. Once you do that rerun this script

--- backgroundimagemessages.js
+++ backgroundimagemessages.js
@@ -1,5 +1,12 @@
+/* 
+IMPORTANT HOW TO USE
+DON'T COPY THIS PART
+
+Send a message of the image you want for your background. Then click on that image. Right click once the image is enlarged. Click copy image link. Once you do that run this script
+
+*/
+
 (function() {
-  alert("Send a message of the image you want for your background. Then click on that image. Right click once the image is enlarged. Click copy image link. Once you do that rerun this script");
   var a = document.querySelector('.chatContent-3KubbW');
   var b = prompt("The message link you copied before running this script","");
   a.style.backgroundImage = "url("+b+")";
