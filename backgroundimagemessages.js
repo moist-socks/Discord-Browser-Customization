9a596141ca467caa1332e5ec1a738cf4a163cc4e
shocz-lol <76273033+shocz-lol@users.noreply.github.com>
2022-03-26 15:31:12 -0500

    Update backgroundimagemessages.js

--- backgroundimagemessages.js
+++ backgroundimagemessages.js
@@ -1,6 +1,6 @@
 (function() {
-  var a = document.querySelector('.chatContent-3KubbW');
   alert("Send a message of the image you want for your background. Then click on that image. Right click once the image is enlarged. Click copy image link. Once you do that rerun this script");
+  var a = document.querySelector('.chatContent-3KubbW');
   var b = prompt("The message link you copied before running this script","");
   a.style.backgroundImage = "url("+b+")";
 })();
