05b3e13b60fbd9568cd47ed15bb194b8d6df50a3
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-05-10 22:55:53 -0500

    Update backgroundimagebest.js

--- backgroundimagebest.js
+++ backgroundimagebest.js
@@ -1,6 +1,8 @@
 document.querySelector('.app-3xd6d0').setAttribute('id','app');
 var appMount = document.getElementById('app-mount');
 var opacity = prompt('Opacity of discord (I.E. 0.7)');
+var img = prompt('Background Image URL');
 document.getElementById('app').style.opacity=opacity;
 appMount.style.backgroundRepeat='no-repeat';
-appMount.style.backgroundSize='contain';
+appMount.style.backgroundSize='cover';
+appMount.style.backgroundImage = "url("+img+")";
