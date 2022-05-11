5fe43b90499162512abd91ad66e08c0e7abe43cb
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-05-10 23:25:16 -0500

    Update backgroundimagebest.js

--- backgroundimagebest.js
+++ backgroundimagebest.js
@@ -1,8 +1,17 @@
-document.querySelector('.app-3xd6d0').setAttribute('id','app');
-var appMount = document.getElementById('app-mount');
-var opacity = prompt('Opacity of discord (I.E. 0.7)');
-var img = prompt('Background Image URL');
-document.getElementById('app').style.opacity=opacity;
-appMount.style.backgroundRepeat='no-repeat';
-appMount.style.backgroundSize='cover';
-appMount.style.backgroundImage = "url("+img+")";
+(function() {
+    'use strict';
+function wrapper() {
+    document.querySelector('.app-3xd6d0').setAttribute('id','app');
+    var appMount = document.getElementById('app-mount');
+    var r = document.querySelector(':root');
+    document.getElementById('app').style.opacity=0.7;
+    appMount.style.backgroundRepeat='no-repeat';
+    appMount.style.backgroundSize='cover';
+    appMount.style.backgroundImage = "url('https://media.discordapp.net/attachments/937866705714159636/973784421436104704/unknown.png?width=1499&height=843')";
+    r.style.setProperty('--background-primary', '#292b2f');
+    r.style.setProperty('--background-secondary', '#292b2f');
+    r.style.setProperty('--background-secondary-alt', '#292b2f');
+    r.style.setProperty('--background-tertiary', '#292b2f');
+}
+setInterval(wrapper,100);
+})();
