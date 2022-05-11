1b3d81a017f34e48769a9c877ae503356a48162f
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-05-10 22:27:38 -0500

    Create backgroundimagebest.js

---
+++ backgroundimagebest.js
@@ -0,0 +1,6 @@
+document.querySelector('.app-3xd6d0').setAttribute('id','app');
+var appMount = document.getElementById('app-mount');
+var opacity = prompt('Opacity of discord (I.E. 0.7)');
+document.getElementById('app').style.opacity=opacity;
+appMount.style.backgroundRepeat='no-repeat';
+appMount.style.backgroundSize='contain';
